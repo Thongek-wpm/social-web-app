cabf97844304836053f47c068730d3ec0fcaba70
Thongek Wogponom <thongek.wpm@gmail.com>
2023-08-19 15:06:26 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   {
-    path: "/Login",
+    path: "/",
     name: "Login",
     component: () => import("@/views/LoginPage.vue"),
   },
